20d9475c818ef1d041a230ed48067b7bda1f5dd9
Mastermind-code <adebowaleadam0@gmail.com>
2023-04-15 12:28:34 +0100

    update snake class

--- main.py
+++ main.py
@@ -1,49 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
-# turtle = Turtle()
-# turtle.shape('square')
-# turtle.color('white')
-# turtle = Turtle()
-# turtle.shape('square')
-# turtle.color('white')
-# turtle.goto(x=-20, y = 0)
-#
-# turtle = Turtle()
-# turtle.shape('square')
-# turtle.color('white')
-# turtle.goto(x=-40, y = 0)
-#
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.title('Snake Game')
-screen.bgcolor('Black')
+screen.bgcolor("black")
+screen.title("My Snake Game")
 screen.tracer(0)
-directions = [(0, 0), (-20, 0), (-40, 0)]
-turtles = []
-for i in directions:
-    turtle = Turtle('square')
-    turtle.color('white')
-    turtle.penup()
-    turtle.goto(i)
-    turtles.append(turtle)
 
-is_on = True
-while is_on:
-    time.sleep(1)
+snake = Snake()
+# food = Food()
+
+# screen.listen()
+# screen.onkey(snake.up, "Up")
+# screen.onkey(snake.down, "Down")
+# screen.onkey(snake.left, "Left")
+# screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
     screen.update()
+    time.sleep(0.1)
 
-    for turtle in range(len(turtles) - 1, 0, -1):
-        new_x = turtles[turtle - 1].xcor()
-        new_y = turtles[turtle - 1].ycor()
-        turtles[turtle].goto(new_x, new_y)
-    turtles[0].forward(40)
-    turtles[0].right(90)
+    # snake.move()
 
 
 screen.exitonclick()
-
-
-class Snake:
-    def __init__(self):
-        
--- snake.py
+++ snake.py
@@ -1,14 +1,30 @@
-import turtle
+from turtle import Turtle
 
-class snake:
+
+class Snake:
     def __init__(self):
-        self.name = 'hello world'
-        directions = [(0, 0), (-20, 0), (-40, 0)]
-        for i in directions:
-            turtle = Turtle('square')
-            turtle.color('white')
-            turtle.penup()
-            turtle.goto(i)
-            turtle.append(turtle)
+        self.segments = []
+        self.directions = [(0, 0), (-20, 0), (-40, 0)]
+        self.create_snake()
+        pass
+
+    def create_snake(self):
+        for i in self.directions:
+            snake = Turtle('square')
+            snake.color('white')
+            snake.penup()
+            snake.goto(i)
+            self.segments.append(snake)
+
+    def move(self, distance):
+        for turtle in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[turtle - 1].xcor()
+            new_y = self.segments[turtle - 1].ycor()
+            self.segments[turtle].goto(new_x, new_y)
+        self.segments[0].forward(20)
+
+
+
+
 
 
